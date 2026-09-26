18e6025d773908d0c3a7f9975a52d7199b36b3ae
Kunsh Agarwal <kunshagarwal94@gmail.com>
2026-09-26 19:36:45 +0530

    removed print function

--- src/BigInt.cpp
+++ src/BigInt.cpp
@@ -1,52 +1,55 @@
-#include"BigInt.hpp"
-#include<iostream>
-BigInt::BigInt(){
+#include "BigInt.hpp"
+#include <iostream>
+BigInt::BigInt()
+{
     digits.push_back(0);
-    isNegative=false;
+    isNegative = false;
 }
-BigInt::BigInt(long long value){
-    if(value<0){
-        isNegative=true;
-        value=-value;
+BigInt::BigInt(long long value)
+{
+    if (value < 0)
+    {
+        isNegative = true;
+        value = -value;
     }
-    else{
-        isNegative=false;
+    else
+    {
+        isNegative = false;
     }
-    if(value==0){
+    if (value == 0)
+    {
         digits.push_back(0);
-        return ;
+        return;
     }
-    while(value>0){
-        digits.push_back(value%10);
-        value/=10;
+    while (value > 0)
+    {
+        digits.push_back(value % 10);
+        value /= 10;
     }
 }
-BigInt::BigInt(const std::string& value){
-    isNegative=false;
-    int start=0;
-    if(value[0]=='-'){
-        isNegative=true;
-        start=1;
+BigInt::BigInt(const std::string &value)
+{
+    isNegative = false;
+    int start = 0;
+    if (value[0] == '-')
+    {
+        isNegative = true;
+        start = 1;
     }
-    for(int i=value.size()-1;i>=start;i--){
-        digits.push_back(value[i]-'0');
+    for (int i = static_cast<int>(value.size()) - 1; i >= start; i--)
+    {
+        digits.push_back(value[i] - '0');
     }
     Normalize();
 }
-void BigInt::Normalize(){
-    while(digits.size()>1 && digits.back()==0){
+void BigInt::Normalize()
+{
+    while (static_cast<int>(digits.size()) > 1 && digits.back() == 0)
+    {
         digits.pop_back();
     }
-    if(digits.size()==1 && digits[0]==0){
-        isNegative=false;
+    if (static_cast<int>(digits.size()) == 1 && digits[0] == 0)
+    {
+        isNegative = false;
     }
-}
-void BigInt::print() const{
-    if(isNegative){
-        std::cout<<'-';
-    }
-    for(int i=digits.size()-1;i>=0;i--){
-        std::cout<<digits[i];
-    }
-    std::cout<<"\n";
 }
